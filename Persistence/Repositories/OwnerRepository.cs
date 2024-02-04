96272411560777d4b76e6282b3437c22c7aeb33f
VeganMeatGuy <steudel.peter@gmail.com>
2024-02-04 16:21:39 +0100

    Add implementations of the persistence layer

---
+++ Persistence/Repositories/OwnerRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories;
+
+internal sealed class OwnerRepository : IOwnerRepository
+{
+    private readonly RepositoryDbContext _dbContext;
+    public OwnerRepository(RepositoryDbContext dbContext) => _dbContext = dbContext;
+    public async Task<IEnumerable<Owner>> GetAllAsync(CancellationToken cancellationToken = default) =>
+        await _dbContext.Owners.Include(x => x.Accounts).ToListAsync(cancellationToken);
+    public async Task<Owner> GetByIdAsync(Guid ownerId, CancellationToken cancellationToken = default) =>
+        await _dbContext.Owners.Include(x => x.Accounts).FirstOrDefaultAsync(x => x.Id == ownerId, cancellationToken);
+    public void Insert(Owner owner) => _dbContext.Owners.Add(owner);
+    public void Remove(Owner owner) => _dbContext.Owners.Remove(owner);
+
+}
