cc43fb0ceff104c73863e1df129d9e277165221f
VeganMeatGuy <steudel.peter@gmail.com>
2024-02-04 19:13:04 +0100

    Add Web project
    not fully functional, docker and other stuff is missing

---
+++ Web/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var webHost = CreateHostBuilder(args).Build();
+
+            await ApplyMigrations(webHost.Services);
+
+            await webHost.RunAsync();
+        }
+
+        private static async Task ApplyMigrations(IServiceProvider serviceProvider)
+        {
+            using var scope = serviceProvider.CreateScope();
+
+            await using RepositoryDbContext dbContext = scope.ServiceProvider.GetRequiredService<RepositoryDbContext>();
+
+            await dbContext.Database.MigrateAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>());
+    }
+}
