3ae268a382bf4e647ab92848f5cc082941563881
VeganMeatGuy <steudel.peter@gmail.com>
2024-02-04 16:00:02 +0100

    Add IRepositoryManager and implementations of the Service Layer

---
+++ Services/ServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Repositories;
+using Services.Abstractions;
+
+namespace Services;
+
+public sealed class ServiceManager
+{
+    private readonly Lazy<IOwnerService> _lazyOwnerService;
+    private readonly Lazy<IAccountService> _lazyAccountService;
+    public ServiceManager(IRepositoryManager repositoryManager)
+    {
+        _lazyOwnerService = new Lazy<IOwnerService>(() => new OwnerService(repositoryManager));
+        _lazyAccountService = new Lazy<IAccountService>(() => new AccountService(repositoryManager));
+    }
+    public IOwnerService OwnerService => _lazyOwnerService.Value;
+    public IAccountService AccountService => _lazyAccountService.Value;
+
+}
