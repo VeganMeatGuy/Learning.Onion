d5256745ec628440b392dc30343f54a4a3611b80
VeganMeatGuy <steudel.peter@gmail.com>
2024-02-04 14:47:04 +0100

    add Contracts Classes and Service.Abstracion Interfaces

---
+++ Contracts/OwnerDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Contracts;
+
+public class OwnerDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public DateTime DateOfBirth { get; set; }
+
+    public string Address { get; set; }
+
+    public IEnumerable<AccountDto> Accounts { get; set; }
+}
